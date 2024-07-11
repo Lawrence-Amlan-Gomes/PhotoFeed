3eed7f6b96be0248508688c11b676fa30f0f01dc
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2024-07-11 15:58:52 +0600

    logo changed

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -6,10 +6,10 @@ const Logo = () => {
     <Link href="/">
       <Image
         className="max-w-[100px] md:max-w-[165px]"
-        src="/lws_logo.png"
-        alt="Lws"
-        height={100}
-        width={165}
+        src="/icon1.png"
+        alt="LAG"
+        height={50}
+        width={50}
       />
     </Link>
   )
